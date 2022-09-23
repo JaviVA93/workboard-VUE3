874573a7df14b6848e75f84ce158db6e9f23d00b
Javi Villar <javier.villar@makingscience.com>
2022-09-23 14:13:04 +0200

    pomo buttons, fix multiple clicks play/pause button, clean a bit

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
+import playSvg from '../assets/play-button-round-icon.svg';
+import pauseSvg from '../assets/pause-button-icon.svg';
+import stopSvg from '../assets/stop-button-round-icon.svg';
+
 let pomo_vars = {
     status: '',
     interval: {
-        minutes: 0,
-        seconds: 10,
+        minutes: 25,
+        seconds: 0,
     },
     timeRemain: {
         seconds: 0,
@@ -17,27 +21,38 @@ const pomoTime = ref<HTMLElement | null>(null);
 const pomoStart = ref<HTMLElement | null>(null);
 const pomoStop = ref<HTMLElement | null>(null);
 
+const cssVars = ref({
+    playPauseBackground: `url("${playSvg}")`,
+    stopBackground: `url("${stopSvg}")`,
+});
+
+let pomoInterval: number | undefined = undefined;
+
 function startPauseResumePomodoro() {
     if (pomo_vars.status === 'running') {
         pomo_vars.status = 'pause';
-        if (pomoStart.value) pomoStart.value.innerText = 'Resume';
+        setPlayPauseButtonBackground('play');
+        if (pomoInterval) clearInterval(pomoInterval);
     }
     else {
         if (pomo_vars.status === '' || pomo_vars.status === 'end')
             pomo_vars.timeRemain = JSON.parse(JSON.stringify(pomo_vars.interval));
+
         pomo_vars.status = 'running';
-        if (pomoStart.value) pomoStart.value.innerText = 'Pause';
-        updatePomodoro();
+
+        setPlayPauseButtonBackground('pause');
+
+        pomoInterval = setInterval(updatePomodoro, 1000);
     }
 }
 
 function stopPomodoro() {
     pomo_vars.status = 'stop';
     pomo_vars.timeRemain = JSON.parse(JSON.stringify(pomo_vars.interval));
-
+    if (pomoInterval) clearInterval(pomoInterval);
     printTime();
 
-    if (pomoStart.value)  pomoStart.value.innerText = 'Start';
+    setPlayPauseButtonBackground('play');
 }
 
 function calculateTimeRemain() {
@@ -50,8 +65,30 @@ function calculateTimeRemain() {
 }
 
 function printTime() {
-    if (pomoTime.value)
-        pomoTime.value.innerText = `${pomo_vars.timeRemain.minutes.toString()}:${pomo_vars.timeRemain.seconds.toString()}`;
+    if (pomoTime.value) {
+        let min = pomo_vars.timeRemain.minutes.toString();
+        min = (min.length === 1) ? `0${min}` : min;
+        let sec = pomo_vars.timeRemain.seconds.toString();
+        sec = (sec.length === 1) ? `0${sec}` : sec;
+
+        pomoTime.value.innerText = `${min}:${sec}`;
+    }
+}
+
+function setPlayPauseButtonBackground(value: string) {
+    try {
+        if (!pomoStart.value)
+            throw 'pomoStart.value is null';
+
+        if (value === 'play')
+            cssVars.value.playPauseBackground = `url("${playSvg}")`;
+        else if (value === 'pause')
+            cssVars.value.playPauseBackground = `url("${pauseSvg}")`;
+        else
+            throw 'setPlayPauseButtonBackground: value is no valid.';
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 function updatePomodoro() {
@@ -59,8 +96,9 @@ function updatePomodoro() {
     Time remaining: ${pomo_vars.timeRemain.minutes}:${pomo_vars.timeRemain.seconds},
     status: ${pomo_vars.status}`);
 
-    if (pomo_vars.status === 'pause' || pomo_vars.status === 'stop' || pomo_vars.status === 'end')
+    if (pomo_vars.status === 'pause' || pomo_vars.status === 'stop' || pomo_vars.status === 'end') {
         return;
+    }
 
     if (pomo_vars.status === 'stop')
         return;
@@ -70,42 +108,45 @@ function updatePomodoro() {
 
     if (pomo_vars.timeRemain.seconds <= 0 && pomo_vars.timeRemain.minutes <= 0) {
         pomo_vars.status = 'end';
-        if (pomoStart.value) pomoStart.value.textContent = 'Start';
+        setPlayPauseButtonBackground('play');
+        if (pomoInterval) clearInterval(pomoInterval);
     }
     else if (pomo_vars.status === 'pause') {
-        if (pomoStart.value) pomoStart.value.textContent = 'Resume'
+        setPlayPauseButtonBackground('play');
         return;
     }
     else {
-        if (pomoStart.value) pomoStart.value.textContent = 'Pause';
-        pomo_vars.status = 'running';
-        setTimeout(() => {
-            updatePomodoro();
-        }, 1000);
+        setPlayPauseButtonBackground('pause');
     }
 }
 onMounted(() => {
     pomo_vars.timeRemain = JSON.parse(JSON.stringify(pomo_vars.interval));
     printTime();
+
+    setPlayPauseButtonBackground('play');
+    console.log(playSvg);
 });
 
 </script>
+
 <template>
     <div class="pomo-card">
         <h1>POMODORO</h1>
-        <span class="pomo_time" ref="pomoTime"></span>
-        <div class="pom_btns_wrapper">
-            <button id="pomo_start" ref="pomoStart" @click="startPauseResumePomodoro">Start</button>
-            <button id="pomo_stop" ref="pomoStop" @click="stopPomodoro">Stop</button>
+        <span class="pomo-time" ref="pomoTime"></span>
+        <div class="pom-btns-wrapper">
+            <button id="pomo-start" ref="pomoStart" @click="startPauseResumePomodoro"></button>
+            <button id="pomo-stop" ref="pomoStop" @click="stopPomodoro"></button>
         </div>
     </div>
 </template>
+
 <style scoped>
 .pomo-card {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    gap: 10px;
 
     background-color: aquamarine;
 
@@ -113,5 +154,30 @@ onMounted(() => {
     padding: 15px;
 
     border-radius: 5px;
+}
+
+.pomo-time {
+    font-size: 32px;
+}
+
+.pom-btns-wrapper {
+    display: flex;
+    gap: 10px;
+}
+
+#pomo-start {
+    width: 35px;
+    height: 35px;
+    background: v-bind('cssVars.playPauseBackground');
+    background-size: contain;
+    border: 0;
+}
+
+#pomo-stop {
+    width: 35px;
+    height: 35px;
+    background: v-bind('cssVars.stopBackground');
+    background-size: contain;
+    border: 0;
 }
 </style>
